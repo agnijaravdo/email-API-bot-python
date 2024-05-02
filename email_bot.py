8ef6c399fddd7c9b54171f1a6ac25992e7256406
Agnija Ravdo <41111594+agnijaravdo@users.noreply.github.com>
2024-05-03 00:52:00 +0300

    add method to update html template with API data

--- email_bot.py
+++ email_bot.py
@@ -1,9 +1,10 @@
 import sys
-from email_validator import validate_email, EmailNotValidError
-from dotenv import load_dotenv
 import os
 import datetime
 import requests
+from email_validator import validate_email, EmailNotValidError
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup as bs
 
 email = sys.argv[1]
 api_provider = sys.argv[2]
@@ -35,14 +36,16 @@ def get_wikimedia_response():
     if response.status_code == 200:
         data = response.json()
         if "births" in data and len(data["births"]) > 0:
-            event_on_this_day = data["births"][0]
-
-            event_on_this_day = data["births"][0]["text"]
-            return event_on_this_day
+            events = []
+            for i in range(min(3, len(data["births"]))):
+                event_text = data["births"][i]["text"]
+                year = data["births"][i]["year"]
+                events.append(f"{year}: {event_text}")
+            return "\n".join(events)
         else:
-            return "The 'births' data is not found or not available"
+            return "The 'births' data is not found or not available."
     else:
-        return "Failed to fetch data:", response.status_code
+        return "Failed to fetch data: HTTP status code " + str(response.status_code)
 
 
 def get_jokes_response():
@@ -50,7 +53,6 @@ def get_jokes_response():
 
     headers = {"Accept": "text/plain"}
     response = requests.get(url, headers=headers)
-    
     if response.status_code == 200:
         return response.text
     else:
@@ -60,14 +62,39 @@ def get_jokes_response():
 def get_data_from_api_providers(api_provider):
     if api_provider == "wikimedia":
         wikimedia_response = get_wikimedia_response()
-        print(wikimedia_response)
+        return wikimedia_response, api_provider
     elif api_provider == "jokes":
         jokes_response = get_jokes_response()
-        print(jokes_response)
+        return jokes_response, api_provider
     else:
-        print(
+        sys.exit(
             f"There is no data for provided API provider. Choose one from {possible_api_providers}"
         )
+
+
+def update_history_newsletter(data):
+    html_file_path = "data/history_newsletter.html"
+    base = os.path.dirname(os.path.abspath(__file__))
+    html = open(os.path.join(base, html_file_path))
+    soup = bs(html, "lxml")
+
+    new_texts = data.splitlines()
+
+    old_h2_tags = soup.find_all("h2")
+
+    if len(old_h2_tags) < len(new_texts):
+        raise ValueError("There are more texts to update than available <h2> tags")
+
+    for index in range(len(new_texts)):
+        old_h2_tags[index].string = f"🎉 {new_texts[index]}"
+
+    with open(html_file_path, "w", encoding="utf-8") as file:
+        file.write(str(soup))
+
+
+def format_data_into_a_message(data, provider):
+    if provider == "wikimedia":
+        update_history_newsletter(data)
 
 
 def main():
@@ -77,7 +104,10 @@ def main():
         )
 
     validate_email_address(email)
-    get_data_from_api_providers(api_provider)
+    data, provider = get_data_from_api_providers(api_provider)
+    print("Api data: ", data)
+    print("Api provider: ", provider)
+    format_data_into_a_message(data, provider)
 
 
 if __name__ == "__main__":
